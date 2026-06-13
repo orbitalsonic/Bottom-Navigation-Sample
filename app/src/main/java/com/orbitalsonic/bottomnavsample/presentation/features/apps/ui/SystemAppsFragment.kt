5d6220315cda2498fdda0e817288f965ddf45123
Hypersoft Inc <social.yaqoob@gmail.com>
2026-06-13 11:44:49 +0500

    Sample

--- app/src/main/java/com/orbitalsonic/bottomnavsample/presentation/features/apps/ui/SystemAppsFragment.kt
+++ app/src/main/java/com/orbitalsonic/bottomnavsample/presentation/features/apps/ui/SystemAppsFragment.kt
@@ -1,9 +1,10 @@
 package com.orbitalsonic.bottomnavsample.presentation.features.apps.ui
 
-import com.orbitalsonic.bottomnavsample.databinding.FragmentMoreBinding
+import com.orbitalsonic.bottomnavsample.databinding.FragmentSystemAppsBinding
 import com.orbitalsonic.bottomnavsample.presentation.base.fragments.BaseFragment
 
-class SystemAppsFragment : BaseFragment<FragmentMoreBinding>(FragmentMoreBinding::inflate) {
+class SystemAppsFragment :
+    BaseFragment<FragmentSystemAppsBinding>(FragmentSystemAppsBinding::inflate) {
 
     override fun onViewCreated() {}
 }
